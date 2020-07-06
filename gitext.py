6739e0372b8e75532c53f9b46abcd499b8bb8810
Iamseahawk <justaguyontheinternet50@gmail.com>
2020-07-06 17:58:53 +0300

    Pushing new command
    In this New commit i have added a dowload repo command, this is for downloading whole repos, and you can add extensions to ignore.

--- gitext.py
+++ gitext.py
@@ -9,7 +9,7 @@ class Download():
 				for files in repos.get_contents(""):
 					if files.path == target_filename:
 						print(files.path)
-						dfile = urllib.request.urlretrieve(files.download_url, local_file_name)
+						urllib.request.urlretrieve(files.download_url, local_file_name)
 						return
 		else:
 			target_repo = user.get_repo(repo)
@@ -18,5 +18,18 @@ class Download():
 			if files.path == target_filename:
 				ext = len(files.path)
 				extension = files.path[ext - 3 :]
-				dfile = urllib.request.urlretrieve(files.download_url, local_file_name + extension)
+				urllib.request.urlretrieve(files.download_url, local_file_name + extension)
 				return
+	def download_repo(token,target_repo,dir_path, ignore_extensions=[]):
+		user = Github(token)
+
+		for repo in user.get_user().get_repos():
+			if repo.full_name == target_repo:
+				for contents in repo.get_contents(""):
+					for bexts in ignore_extensions:
+						if contents.path.endswith(bexts):
+							pass
+						else:
+							print(f"downloading {contents.path}")
+							urllib.request.urlretrieve(contents.download_url,f'{dir_path}/{contents.path}')
+
