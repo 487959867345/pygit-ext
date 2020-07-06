6fb0c107fb9484a41b048c77d1fe51e675c2a357
Iamseahawk <justaguyontheinternet50@gmail.com>
2020-07-06 03:34:27 +0300

    Create gitext.py

---
+++ gitext.py
@@ -0,0 +1,22 @@
+import urllib.request
+from github import Github
+
+def download_file(token, target_filename,local_file_name, repo=None):
+	user = Github(token)
+	
+	if repo == None:
+		for repos in user.get_user().get_repos():
+			for files in repos.get_contents(""):
+				if files.path == target_filename:
+					print(files.path)
+					dfile = urllib.request.urlretrieve(files.download_url, local_file_name)
+					return
+	else:
+		target_repo = user.get_repo(repo)
+
+	for files in target_repo.get_contents(""):
+		if files.path == target_filename:
+			dfile = urllib.request.urlretrieve(files.download_url, local_file_name)
+			return
+
+
