5e39fb98b3ff8c6f3cf6066a8589c45a0caf5e98
Iamseahawk <justaguyontheinternet50@gmail.com>
2020-07-06 13:31:27 +0300

    Update gitext.py
    Simple restructering of the module and how dowloads are handled

--- gitext.py
+++ gitext.py
@@ -1,25 +1,24 @@
 import urllib.request
 from github import Github
-
-def download_file(token, target_filename,local_file_name, repo=None):
-	user = Github(token)
+class Dowload():
+	def download_file(token, target_filename,local_file_name, repo=None):
+		user = Github(token)
 	
-	if repo == None:
-		for repos in user.get_user().get_repos():
-			for files in repos.get_contents(""):
-				if files.path == target_filename:
-					print(files.path)
-					dfile = urllib.request.urlretrieve(files.download_url, local_file_name)
-					return
-	else:
-		target_repo = user.get_repo(repo)
+		if repo == None:
+			for repos in user.get_user().get_repos():
+				for files in repos.get_contents(""):
+					if files.path == target_filename:
+						print(files.path)
+						dfile = urllib.request.urlretrieve(files.download_url, local_file_name)
+						return
+		else:
+			target_repo = user.get_repo(repo)
 
-	for files in target_repo.get_contents(""):
-		if files.path == target_filename:
-			ext = len(files.path)
-			extension = files.path[ext - 3 :]
-			print(extension)
-			dfile = urllib.request.urlretrieve(files.download_url, local_file_name + extension)
-			return
+		for files in target_repo.get_contents(""):
+			if files.path == target_filename:
+				ext = len(files.path)
+				extension = files.path[ext - 3 :]
+				dfile = urllib.request.urlretrieve(files.download_url, local_file_name + extension)
+				return
 
 
